c3e6c53cd4d3fdd83225d7a9d21820f9ba5f53db
andyrude <anirudh.mishra_cs19@gla.ac.in>
2022-03-15 19:53:02 +0530

    Commit

---
+++ src/ReturningIndex.java
@@ -0,0 +1,27 @@
+public class ReturningIndex {
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 5};
+        int target = 78;
+        index( nums, target);
+    }
+
+    private static void index(int[] nums, int target) {
+        int i = 0;
+        int j = 0;
+        for ( i = 0; i < nums.length - 1; i++){
+            for (  j =1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    break;
+                }
+            }
+            if ( j != nums.length){
+                break;
+            }
+        }
+        if ( i == nums.length - 1){
+            System.out.println(-1);
+        }else {
+            System.out.println( "[" + i + "," + j+ "]");
+        }
+    }
+}
---
+++ src/SecondMaxVal.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class SecondMaxVal {
+    public static void main(String[] args){
+        int[] arr = { 2,2,1,4,5};
+        secondMaxVal(arr);
+    }
+
+    private static void secondMaxVal(int[] arr) {
+        HashMap< Integer, Integer> map = new HashMap<>();
+        for ( int i = 0; i < arr.length; i++){
+            if ( map.containsKey(arr[i])){
+                int j = map.get( arr[i]);
+                j += 1;
+                map.replace( arr[i], j);
+            }else {
+                map.put( arr[i],1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+        System.out.println( list.get(list.size() - 2));
+    }
+
+    }
